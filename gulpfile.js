5f330bd2b95a104876d4233c41d59bc6ac950a8c
Greg Pazo <gregpazo@gmail.com>
2015-10-20 11:37:01 -0400

    fix typo in gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -16,7 +16,7 @@ var config = {
 		html: './src/*.html',
 		js: './src/**/*.js',
 		css: [
-					'node_modules/bootstrap/dist/css/boostrap.min.css',
+					'node_modules/bootstrap/dist/css/bootstrap.min.css',
 					'node_modules/bootstrap/dist/css/bootstrap-theme.min.css'
 		],
 		dist: './dist',
